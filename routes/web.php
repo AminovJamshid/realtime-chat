11627d1ac2243f8b024081efab70b7a495380241
AminovJamshid <aminovjamshid777@gmail.com>
2024-11-23 12:41:27 +0500

    MINOR: working Long pooling chat elements

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $users = \App\Models\User::all()->toArray();
+    return view('welcome', compact('users'));
+});
+
+Route::post('/message', function (Request $request) {
+    return $request->all();
 });
 
 Route::get('/dashboard', function () {
